7a9877cd3c2259c96935e69c5e346eae317ef826
Heli Xu <xuheli91@gmail.com>
2022-10-14 15:30:32 -0400

    change Roxygen in group_mean.R to update function documentation
    leave a line black between title and description of the function in Roxygen commetns

--- R/group_mean.R
+++ R/group_mean.R
@@ -1,4 +1,5 @@
 #' Summarise mean value for each group
+#'
 #' Like summarise(), group_mean()creates a new data frame. It's a combination of grouping by one variable and caculating measurements in each group. It's also useful in calculating an average for technical repeats, such as immunohistochemitry quantification (where you have multiple measurements for one sample).
 #'
 #' @param data input dataset, including at least a column of grouping information and a column of measurements
